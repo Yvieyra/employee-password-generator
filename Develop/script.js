10921f940270de225a7470e827dbf1ad265093e4
Ying Yang <vieyrayessenia8@gmail.com>
2022-11-10 00:08:26 -0700

    added comments to code

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,30 @@
+/*
+var password = '';
+let userChoice = 50;
+
+for (var i = 0; i < totalCharcters.length; i++) {
+    var randomNumber = Math.floor(Math.random() * lowerCase .length);
+    var letter = lowerCase[randomNumber];
+
+   if (userChoice > password.length) {
+        password += letter;
+   }
+    
+}
+
+console.log("Password: ", password);
+
+var password = "gfshfhsfhs";
+var userChoice = 8;
+var guarantted = "jK9";
+
+var actualPassword = password.slice(0, userChoice - guarantted.length) + guarantted;
+
+*/
+
 // Assignment code here
-var lowerCase = ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
-var lowerCase = ("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
+var lowerCase =("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
+var upperCase = ("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
 var specialChar = ("!","@","*","&","+","#","%","." )
 var numbers = ("0","1","2","3","4","5","6","7","8","9")
 
@@ -14,15 +38,28 @@ document.getElementById("generate").onclick = function () {
     console.log(passwordLetters);
   var passwordSymbols = window.prompt ("Got it! Would you like your password to include special characters? yes or no.");
     console.log(passwordSymbols);
-  var passwordNumbers = window.prompt ("Would you like your password to include numbers? yes or no."); 
+  var passwordNumbers = window.prompt ("Would you like your password to include numbers? yes or no."); //does each window.prompt need an if statement pulling in any array to compare?
     console.log(passwordNumbers);
   } else {
     window.alert ("Please try again, value must be no less than 8 and no more than 125.")
   }
 }
 
+//The password should be random based on their answers - DONE
+// we need to guarentee the user what they input - Progress
+
+//The password needs to stay within user lenth choice (.length)
+//What if the user said no to all options offered? 
+
+
 function generatePassword() { 
  //thinking about writing my Math.floor.Math.random equation in here. 
+ if (passwordLetters === "yes") {
+ 
+ }
+ for (var i = 0; i<= passwordLength; i++) {
+  var numbers = Math.floor(Math.random())
+ }
 }
 
 
