74c741c4efd65bd687c52382fafb9754e29cdb45
Ying Yang <vieyrayessenia8@gmail.com>
2022-11-11 12:37:48 -0700

    added content to my README.md file

--- Develop/script.js
+++ Develop/script.js
@@ -1,35 +1,16 @@
-/*
-var password = '';
-let userChoice = 50;
 
-for (var i = 0; i < totalCharcters.length; i++) {
-    var randomNumber = Math.floor(Math.random() * lowerCase .length);
-    var letter = lowerCase[randomNumber];
-
-   if (userChoice > password.length) {
-        password += letter;
-   }
-    
-}
-
-console.log("Password: ", password);
-
-var password = "gfshfhsfhs";
-var userChoice = 8;
-var guarantted = "jK9";
-
-var actualPassword = password.slice(0, userChoice - guarantted.length) + guarantted;
-
-*/
-
+//var actualPassword = password.slice(0, userChoice - guarantted.length) + guarantted;
 // Assignment code here
-var lowerCase =("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
-var upperCase = ("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
-var specialChar = ("!","@","*","&","+","#","%","." )
-var numbers = ("0","1","2","3","4","5","6","7","8","9")
+var lowerCase =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var specialChar = ["!","@","*","&","+","#","%","." ]
+var numbers = ["0","1","2","3","4","5","6","7","8","9"]
+var totalCharacters = lowerCase.concat(upperCase, specialChar,numbers, upperCase, specialChar, lowerCase);
+var password = '';
+
 
 //Do i need the .onclick code here or just alerts and instead use the Eventlistener code piece below?
-document.getElementById("generate").onclick = function () {
+// document.getElementById("generate").onclick = function () {
   window.confirm ("Let's get started on creating your secure password!");
   var passwordLength = window.prompt ("Enter the number of characters you want your password to be. A minimum of 8 characters and a maximum of 128 characters.");
   if (passwordLength >= 8 && passwordLength <= 128)  {
@@ -38,12 +19,12 @@ document.getElementById("generate").onclick = function () {
     console.log(passwordLetters);
   var passwordSymbols = window.prompt ("Got it! Would you like your password to include special characters? yes or no.");
     console.log(passwordSymbols);
-  var passwordNumbers = window.prompt ("Would you like your password to include numbers? yes or no."); //does each window.prompt need an if statement pulling in any array to compare?
+  var passwordNumbers = window.prompt ("Would you like your password to include numbers? yes or no."); 
     console.log(passwordNumbers);
   } else {
     window.alert ("Please try again, value must be no less than 8 and no more than 125.")
   }
-}
+// }
 
 //The password should be random based on their answers - DONE
 // we need to guarentee the user what they input - Progress
@@ -52,15 +33,19 @@ document.getElementById("generate").onclick = function () {
 //What if the user said no to all options offered? 
 
 
-function generatePassword() { 
+// function generatePassword() { 
+  generateBtn.addEventListener("click", writePassword); {
  //thinking about writing my Math.floor.Math.random equation in here. 
- if (passwordLetters === "yes") {
- 
- }
- for (var i = 0; i<= passwordLength; i++) {
-  var numbers = Math.floor(Math.random())
- }
-}
+//  if (passwordLetters === "yes") {
+  for (var i = 0; i < totalCharacters.length; i++) {
+    var randomNumber = Math.floor(Math.random() * lowerCase.length);
+    var letter = lowerCase[randomNumber];
+
+   if (totalCharacters > passwordLength.length) {
+        password += letter;
+   }
+    
+  }}
 
 
 // Get references to the #generate element
@@ -75,4 +60,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # employee-password-generator
-Create a unique, and strong password that follows specific criteria
+
+## Description
+This webpage is intented to create a unique, and strong password that follows specific criteria selected by the user for the user. 
+
+## Installation 
+This webpage does not need any installation, all the functions are on the webpage. User may need to allow window pop-ups. 
+
+## Usage
+The user is going to be directed to a webpage that has a window confirmation, stating: "Let's get started on creating your secure password". Once the user selects okay there will be window alerts for the user to whether or not they want their generated password to include numbers, paswords, and/or special characters. Once the user is done with the window alerts they should be able to click the "generate password" button. Once the button has been selected the program will display the password in the text area box provided. 
+
+## Link to site
+
+## Screenshot 
