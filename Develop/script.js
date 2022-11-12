aaba633de4a5676ea8ab9fa75d4bec1e262ea70d
Ying Yang <vieyrayessenia8@gmail.com>
2022-11-12 01:31:06 -0700

    formatted code in script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,40 +1,41 @@
 
 
 // variable choices for code here
-var lowerCase =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-var specialChar = ["!","@","*","&","+","#","%","." ]
-var numbers = ["0","1","2","3","4","5","6","7","8","9"]
-var totalCharacters = lowerCase.concat(upperCase, specialChar,numbers, upperCase, specialChar, lowerCase);
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var specialChar = ["!", "@", "*", "&", "+", "#", "%", "."]
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var totalCharacters = lowerCase.concat(upperCase, specialChar, numbers, upperCase, specialChar, lowerCase);
 var password = '';
 
 
 // document.getElementById("generate").onclick = function () {
-  window.confirm ("Let's get started on creating your secure password!");
-  var passwordLength = window.prompt ("Enter the number of characters you want your password to be. A minimum of 8 characters and a maximum of 128 characters.");
-  if (passwordLength >= 8 && passwordLength <= 128)  {
-    // console.log(passwordLength);
-  var passwordLetters = window.prompt ("Great! Do you want your password to include lowercase and uppercase letters? yes or no."); 
-    console.log(passwordLetters);
-  var passwordSymbols = window.prompt ("Got it! Would you like your password to include special characters? yes or no.");
-    // console.log(passwordSymbols);
-  var passwordNumbers = window.prompt ("Would you like your password to include numbers? yes or no."); 
-    // console.log(passwordNumbers);
-  } else {
-    window.alert ("Please try again, value must be no less than 8 and no more than 125.")
-  }
+window.confirm("Let's get started on creating your secure password!");
+var passwordLength = window.prompt("Enter the number of characters you want your password to be. A minimum of 8 characters and a maximum of 128 characters.");
+if (passwordLength >= 8 && passwordLength <= 128) {
+  // console.log(passwordLength);
+  var passwordLetters = window.prompt("Great! Do you want your password to include lowercase and uppercase letters? yes or no.");
+  console.log(passwordLetters);
+  var passwordSymbols = window.prompt("Got it! Would you like your password to include special characters? yes or no.");
+  // console.log(passwordSymbols);
+  var passwordNumbers = window.prompt("Would you like your password to include numbers? yes or no.");
+  // console.log(passwordNumbers);
+} else {
+  window.alert("Please try again, value must be no less than 8 and no more than 125.")
+}
 
 //The below code is what is needed to generate the password and write password to the text reas on the webpage.
-function generatePassword() { 
-if (passwordLength === numbers.splice) {
-// for (var i = 0; i < totalCharacters.length; i++) {
-//   var randomNumber = Math.floor(Math.random() * lowerCase.length);
-//   var letter = lowerCase[randomNumber];
+function generatePassword() {
+  if (passwordLength === numbers.splice) {
+    // for (var i = 0; i < totalCharacters.length; i++) {
+    //   var randomNumber = Math.floor(Math.random() * lowerCase.length);
+    //   var letter = lowerCase[randomNumber];
 
-//  if (totalCharacters > passwordLength.length) {
-//       password += letter;
-// 
-}}
+    //  if (totalCharacters > passwordLength.length) {
+    //       password += letter;
+    // 
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("generate");
